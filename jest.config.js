5a011a8beeca74f6bd64907f2d498d7857c7b1d0
Matheus Coronado <matheuscoronado48@gmail.com>
2026-06-18 20:35:38 -0300

    chore(config): adiciona arquivos de configuração do ambiente de testes e dependências

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
-   module.exports = {
-     preset: 'jest-expo',
-     transformIgnorePatterns: [
-       'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
-     ],
-     setupFilesAfterEnv: ["@testing-library/react-native/extend-expect"]
-   };
+module.exports = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native(-community)?|@react-navigation|@react-native/js-polyfills)',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+};
